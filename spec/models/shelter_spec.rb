94e258b6018f9295d7514057d68156c07f062176
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-24 13:46:10 -0600

    Update shelter model test should have many reviews Testing instance method pet_count

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
-describe Shelter, type: :model do
-  
+Rspec.describe Shelter, type: :model do
+
   describe "relationships" do
     it {should have_many :pets}
+    it {should have_many :reviews}
+  end
+
+
+  describe "instance methods" do
+    it "can return pet count for individual shelter" do
+
+    expect(@shelter_1.pet_count).to eq(2)
+    expect(@shelter_2.pet_count).to eq(2)
+    end 
   end
 end
