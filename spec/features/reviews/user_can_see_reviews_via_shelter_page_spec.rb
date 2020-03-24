09c9afb9832ce7438d3e4a3909e1bd209d597567
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-24 12:08:33 -0600

    Add comment to whiteboard for future reference

--- spec/features/reviews/user_can_see_reviews_via_shelter_page_spec.rb
+++ spec/features/reviews/user_can_see_reviews_via_shelter_page_spec.rb
@@ -11,13 +11,4 @@ RSpec.describe 'As a visitor' do
     expect(page).to have_content(@review_2.content)
   end
 
-  # it 'I can see shelter reviews with no image provided' do
-  #
-  #   visit "/shelters/#{@shelter_2.id}"
-  #
-  #   expect(page).to have_content(@review_3.title)
-  #   expect(page).to have_content(@review_3.rating)
-  #   expect(page).to have_content(@review_3.content)
-  #   expect(page).to_not have_content(@review_3.image)
-  # end
 end
