5737e15e57eaf9547c6944093305c1ec43df02f2
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-24 14:49:41 -0600

    Add update to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -16,7 +16,18 @@ class ReviewsController < ApplicationController
   def edit
     @review = Review.find(params[:id])
   end
-  
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+    if review.save
+      redirect_to "/shelters/#{review.shelter.id}"
+    else
+      flash[:notice] = "All fields are required"
+      redirect_to "/reviews/#{review.id}/edit"
+    end
+  end
+
   private
 
   def review_params
