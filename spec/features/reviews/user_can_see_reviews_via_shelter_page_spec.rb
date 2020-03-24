85a25fa0db832fd4d2219bed5e6505e7f1ae2e9b
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-23 23:47:19 -0600

    Create test for user story 3 and added a more robust test for user story 2

--- spec/features/reviews/user_can_see_reviews_via_shelter_page_spec.rb
+++ spec/features/reviews/user_can_see_reviews_via_shelter_page_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   it 'I can see shelter reviews via the shelter show page' do
-    review_1 = @shelter_1.reviews.create({ title: "This is Awesome",
-                  rating: "9",
-                  content: "This is some content about the shelter",
-                  image: 'https://i.imgur.com/wKls5bM.png'})
-    review_2 = @shelter_1.reviews.create({ title: "This is Great",
-                                           rating: "8",
-                                           content: "This is some content about the shelter",
-                                           image: 'https://i.imgur.com/wKls5bM.png'})
 
     visit "/shelters/#{@shelter_1.id}"
 
-    expect(page).to have_content(review_1.title)
-    expect(page).to have_content(review_1.rating)
-    expect(page).to have_content(review_1.content)
-    expect(page).to have_content(review_2.content)
-    save_and_open_page
+    expect(page).to have_content(@review_1.title)
+    expect(page).to have_content(@review_1.rating)
+    expect(page).to have_content(@review_1.content)
+    expect(page).to have_content(@review_2.content)
   end
+
+  # it 'I can see shelter reviews with no image provided' do
+  #
+  #   visit "/shelters/#{@shelter_2.id}"
+  #
+  #   expect(page).to have_content(@review_3.title)
+  #   expect(page).to have_content(@review_3.rating)
+  #   expect(page).to have_content(@review_3.content)
+  #   expect(page).to_not have_content(@review_3.image)
+  # end
 end
