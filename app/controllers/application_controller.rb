666c40fbaaf012125960e872ae18b43ade2df2a6
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-25 15:56:46 -0600

    User story 8 Test passing. Need to implement logic to counter favorites once we have pets being favorited.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def favorites_universal
+     cookies[:favorites] ||= []
+     @favorited_pets_counter = cookies[:favorites].length
+  end
 end
---
+++ spec/features/favorites/user_can_see_favorites_counter_on_nav_bar_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "favorites counter view", type: :feature do
+  it "can see favorites counter on pets pages" do
+
+    visit '/pets'
+
+    expect(page).to have_link('Favorites')
+  end
+
+  it "can see favorites counter on pets shelter pages " do
+
+    visit "/shelters/#{@shelter_1.id}/pets"
+
+    expect(page).to have_link('Favorites')
+  end
+
+  it "can see favorites counter on home pages " do
+
+    visit "/"
+
+    expect(page).to have_link('Favorites')
+  end
+end
