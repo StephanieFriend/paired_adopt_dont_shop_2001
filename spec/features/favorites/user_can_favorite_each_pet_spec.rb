3b2ab8603a63ba4928df3d77da84d4fc394136fc
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-25 17:41:37 -0600

    Test for user story 9

---
+++ spec/features/favorites/user_can_favorite_each_pet_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Able to favorite pet', type: :feature do
+    it 'able to favorite pet on pets show page and favorite counter increases' do
+
+      visit "/pets/#{@pet_1.id}"
+
+      expect(page).to have_content("Favorites (0)")
+
+      click_on('Favorite This Pet')
+
+      expect(current_path).to eq("/pets/#{@pet_1.id}")
+      expect(page).to have_content("This pet has been added to your favorites!")
+      expect(page).to have_content("Favorites (1)")
+
+      visit "/pets/#{@pet_2.id}"
+
+      expect(page).not_to have_content("This pet has been added to your favorites!")
+      expect(page).to have_content("Favorites (1)")
+
+      click_on('Favorite This Pet')
+
+      expect(current_path).to eq("/pets/#{@pet_2.id}")
+      expect(page).to have_content("This pet has been added to your favorites!")
+      expect(page).to have_content("Favorites (2)")
+    end
+  end
