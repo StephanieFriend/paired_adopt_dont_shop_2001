cf673c6b9873d604319c8c04f6c75bc014cbed70
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-25 22:45:58 -0600

    Add test for favorites Multiple favorites Removing favorites

---
+++ spec/features/favorites/user_can_only_favorite_pet_once_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+
+RSpec.describe "After I've favorited a pet" do
+  it "When I visit that pet's show page I don't see favorite link but one to remove it from favorites" do
+
+    visit "/pets/#{@pet_1.id}"
+
+    click_link ('Favorite This Pet')
+
+    expect(page).not_to have_link('Favorite This Pet')
+    expect(page).to have_link('Un-favorite This Pet')
+  end
+
+  it "it is removed from favorites when I click link (Remove From Favorites)" do
+
+    visit "/pets/#{@pet_1.id}"
+
+    click_on('Favorite This Pet')
+
+    expect(page).to have_content('Favorite Count: 1')
+
+    click_on('Un-favorite This Pet')
+
+    expect(current_path).to eq("/pets/#{@pet_1.id}")
+    expect(page).to have_content("This pet has been removed from your favorites")
+    expect(page).to have_link('Favorite This Pet')
+    expect(page).not_to have_link('Un-favorite This Pet')
+    expect(page).to have_content('Favorite Count: 0')
+ end
+end
