e30bc36cde130a04ebf47c9b9a5c1e8612417fc8
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-27 14:36:06 -0600

    continuation of render - Steph

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -44,7 +44,17 @@ class PetsController < ApplicationController
     redirect_to '/pets'
   end
 
-  def favorite
+  def favorites
+    all_pets = Pet.all
+    if all_pets != nil && session[:favorites] != nil
+      @pets = all_pets.find_all { |pet| session[:favorites].include?(pet.id.to_s)}
+    else
+      @pets = []
+    end
+  end
+
+
+  def toggle_favorite
     if session[:favorites] == nil
       session[:favorites] = [params[:id]]
       flash[:favorited] = "#{get_pet_info.name} has been added to your favorites!"
