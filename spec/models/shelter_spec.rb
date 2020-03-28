0f45bb50fe8da5ad5a46b6086976a037ba84a67a
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-03-28 17:44:38 -0600

    Add test for models and application process

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Shelter, type: :model do
 
   describe "relationships" do
-    it {should have_many :pets}
-    it {should have_many :reviews}
+    it { should have_many :pets }
+    it { should have_many :reviews }
   end
 
   describe "instance methods" do
