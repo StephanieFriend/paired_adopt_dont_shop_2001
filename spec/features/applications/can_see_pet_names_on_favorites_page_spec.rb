8643d318f8e7660637107d9266a915f60ee82685
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-28 18:56:48 -0600

    Wrote test for user 18 story

---
+++ spec/features/applications/can_see_pet_names_on_favorites_page_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  it 'I see a section on the favorites page that has the applied for pets name with links to their show page' do
+
+    visit "/pets/#{@pet_3.id}"
+
+    click_on('Favorite This Pet')
+
+    visit '/favorites'
+
+    click_link 'Adopt Favorited Pets'
+
+    find("#checkbox-#{@pet_3.id}").set(true)
+
+    fill_in :name, with: "Test Address"
+    fill_in :address, with: "Test Address"
+    fill_in :city, with: "Test City"
+    fill_in :state, with: "Test State"
+    fill_in :zip, with: "Test Zip"
+    fill_in :phone_number, with: "Test Number "
+    fill_in :description, with: "Test Description"
+
+    click_button 'Submit'
+
+    expect(current_path).to eq("/favorites")
+    expect(page).to have_content(@pet_3.name)
+    expect(page).to_not have_content(@pet_2.name)
+    expect(page).to_not have_content(@pet_3.sex)
+  end
+end
