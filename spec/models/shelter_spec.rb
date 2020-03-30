34d70a98f1a94bdfc4fcd1fb042b6d76ee6df703
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-30 17:07:26 -0600

    Create model tests for count of reviews and average reviews

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe Shelter, type: :model do
     expect(@shelter_1.pet_count).to eq(2)
     expect(@shelter_2.pet_count).to eq(2)
     end
+
+    it "can return review count for individual shelter" do
+
+      expect(@shelter_1.review_count).to eq(2)
+      expect(@shelter_2.review_count).to eq(1)
+    end
+
+    it "can return review average for individual shelter" do
+
+      expect(@shelter_1.review_average).to eq(3.5)
+    end
   end
 end
