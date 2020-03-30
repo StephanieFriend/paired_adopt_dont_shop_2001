fc76ffed159f19a6cf8fb1dc74d07d16470d2f18
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-30 13:21:30 -0600

    Adjusted previous delete test to work with the new adjusted destroy method

--- spec/features/shelters/user_can_delete_each_shelter_spec.rb
+++ spec/features/shelters/user_can_delete_each_shelter_spec.rb
@@ -1,7 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe "shelter delete process", type: :feature do
+  before(:each) do
+    @application_1 = Application.create( name: "Mary Margret",
+                                         address: "123 Charming Ln.",
+                                         city: "Storybrooke",
+                                         state: "ME",
+                                         zip: "54321",
+                                         phone_number: "123-456-7890",
+                                         description: "I'm Snow White. Duh.")
+
+    PetApplication.create({pet_id: @pet_1.id, application_id: @application_1.id})
+  end
+
   it "can complete deletion with DELETE and redirect" do
+
+    visit "/applications/#{@application_1.id}"
+
+    expect(page).to have_content("Approve Application For: #{@pet_1.name}")
 
     visit "/shelters/#{@shelter_1.id}"
 
