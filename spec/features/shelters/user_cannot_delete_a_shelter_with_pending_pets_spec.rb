24e163c0d409dd94da62e6b5c1c8238f0490c962
Stephanie Friend <stephaniefriend91@gmail.com>
2020-03-30 13:19:52 -0600

    Wrote test for user story 26

---
+++ spec/features/shelters/user_cannot_delete_a_shelter_with_pending_pets_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor' do
+  before(:each) do
+    @application_1 = Application.create( name: "Mary Margret",
+                                         address: "123 Charming Ln.",
+                                         city: "Storybrooke",
+                                         state: "ME",
+                                         zip: "54321",
+                                         phone_number: "123-456-7890",
+                                         description: "I'm Snow White. Duh.")
+
+    PetApplication.create({pet_id: @pet_1.id, application_id: @application_1.id})
+  end
+
+  it 'I cannot delete a shelter if a pet has an approved application' do
+
+    visit "/applications/#{@application_1.id}"
+
+    click_link "Approve Application For: #{@pet_1.name}"
+
+    visit "/shelters/#{@shelter_1.id}"
+
+    click_link 'Delete Shelter'
+
+    expect(page).to have_content("Cannot Delete Shelter. Pets With Pending Applications.")
+  end
+end
